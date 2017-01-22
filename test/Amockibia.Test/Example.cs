21c0b66678218444689abe7e0d73488b28c06191
Wang Tao <miffyliye@gmail.com>
2017-01-22 21:02:14 +0800

    test core facilities instead of extensions

--- test/Amockibia.Test/Example.cs
+++ test/Amockibia.Test/Example.cs
@@ -1,6 +1,10 @@
-﻿using Amockibia.Extensions;
+﻿using Amockibia.Rule;
+using Amockibia.Rule.Builder;
+using Amockibia.Test.Utilities;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -8,12 +12,41 @@ namespace Amockibia.Test
 {
     public class Example : TestBase
     {
+        private class HttpMethodMatcher : IRequestMatchable
+        {
+            private HttpMethod Method { get; }
+
+            public HttpMethodMatcher(string serverId, HttpMethod method)
+            {
+                Method = method;
+            }
+
+            public bool Matches(HttpRequest request)
+            {
+                return request.Method == Method.ToString();
+            }
+        }
+
+        private class HttpMethodMatchRuleBuilder : IRuleBuildable
+        {
+            private HttpMethod Method { get; }
+            public HttpMethodMatchRuleBuilder(HttpMethod method)
+            {
+                Method = method;
+            }
+            public RequestHandler Build(string serverId)
+            {
+                return new RequestHandler(new HttpMethodMatcher(serverId, Method), new StatusCodeOnlyResponder(HttpStatusCode.OK));
+            }
+        }
+
+
         [Theory]
         [InlineData(true)]
         [InlineData(false)]
         public async Task should_return_not_implemented_when_request_not_matched(bool isInMemoryHost)
         {
-            Server.Stub(When.Get("OK").RespondOK());
+            Server.Stub(new HttpMethodMatchRuleBuilder(HttpMethod.Delete));
             var client = SelectHttpClient(isInMemoryHost);
 
             var response = await client.GetAsync("NotImplemented");
@@ -22,14 +55,14 @@ namespace Amockibia.Test
         }
 
         [Theory]
-        [InlineData(true, "OK")]
-        [InlineData(false, "OK")]
-        public async Task should_return_stub_when_request_matched(bool isInMemoryHost, string relativeUri)
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task should_return_stub_when_request_matched(bool isInMemoryHost)
         {
-            Server.Stub(When.Get(relativeUri).RespondOK());
+            Server.Stub(new HttpMethodMatchRuleBuilder(HttpMethod.Delete));
             var client = SelectHttpClient(isInMemoryHost);
 
-            var response = await client.GetAsync(relativeUri);
+            var response = await client.DeleteAsync("");
 
             response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
